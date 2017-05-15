2f63011d23396ac006859744938cfd8eb820f346
Tyler Rhorick <trhorick@gmail.com>
2017-05-15 22:25:48 +0800

    4th draft

--- sketch.js
+++ sketch.js
@@ -1,23 +1,21 @@
 "use strict";
 var p;
-var DEBUG_MODE = true;
 var particles = [];
 var canvas, img;
 var s;
 var grow;
 var on;
 
-
+// Special thanks to Maggie Wlash. Check out her website at http://www.maggiemwalsh.com/
 
 function setup() {
 textFont("Questrial");
  canvas = createCanvas(window.innerWidth, window.innerHeight);
   img= loadImage("./assets/logo.png");
-  if (DEBUG_MODE) {
-    //frameRate(5);
-  }
-  for (var i = 0; i < 100; i++) {
-    particles.push(new Particle(100, height / 2));
+  
+  for (var i = 0; i<= 100; i++) {
+    particles.push(new Particle(150, height));
+    particles.push(new Particle2(100,height));
   }
   rectMode(CENTER);
   s=0;
@@ -37,17 +35,21 @@ function draw() {
   }
   if(mouseX>=((width/2)-100) && mouseX<=(width/2)+100 && mouseY<=(height/2)+100 && mouseY>=(height/2)-100){
     on=true;
+    for (var i = 0; i < particles.length; i++) {
+    particles[i].modify();
+    particles[i].display();
+    }
     if(mouseIsPressed){
       window.open("./home.html","_self");
     }
   }
   else{
     on=false;
-  }
-  for (var i = 0; i < particles.length; i++) {
+      for (var i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].display();
 
+  }
   }
   imageMode(CENTER);
   rectMode(CENTER);
@@ -62,12 +64,12 @@ function draw() {
   fill(255);
   textAlign(CENTER);
   textSize(25);
-  text("Tyler Rhorick's Portfolio",width/2,height/2+150);
+  text("Tyler Rhorick's Portfolio",width/2,height/2+200);
 }
   image(img,width/2,height/2,200,200);
 }
-var DEBUG_MODE = true;
 var mouseVec;
+var randomVec;
 
 class Particle {
   constructor(x, y) {
@@ -75,8 +77,8 @@ class Particle {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(1, 0);
-    this.accadj = random(0.01, 0.4);
-    this.velReduction = random(0.7, 0.9);
+    this.accadj = random(0.01, .4);
+    this.velReduction = .7;
     
 
   }
@@ -84,27 +86,78 @@ class Particle {
   update() {
     mouseVec = createVector(mouseX, mouseY);
     this.acc = p5.Vector.sub(mouseVec, this.pos);
-    this.acc.mult(0.7);
+    this.acc.mult(2);
     this.vel.mult(this.velReduction);
     this.acc.mult(this.accadj);
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     
-
-
+  }
+  modify() {
+    this.pos=createVector(random(width/2-150,width/2+150),random(height/2-150,height/2+150));
+    this.acc=createVector(-.05,.05);
+    this.velocity= createVector(random(-10,10),random(-10,10));
+    this.vel.add(this.acc);
+    this.pos.add(this.velocity);
+    // mouseVec = createVector(mouseX, mouseY);
+    // this.acc = p5.Vector.sub(mouseVec, this.pos);
+    // randomVec= p5.Vector.random2D();
+    // this.acc=p5.Vector.sub(randomVec,this.pos);
+    // this.acc.mult(2);
+    // this.vel.mult(this.velReduction);
+    // this.acc.mult(this.accadj);
+    // this.vel.add(this.acc);
+    
   }
   display() {
     push();
     translate(this.pos.x, this.pos.y);
     noStroke();
     fill(255,random(60,64),random(120,129), random(5,255));
-    ellipse(0, 0, this.dia, this.dia);
+    rect(0, 0, this.dia, this.dia);
 
-    if (DEBUG_MODE) {
-      //stroke(255, 255, 255);
-      //strokeWeight(3);
-      //line(0, 0, this.vel.x, this.vel.y);
-    }
+    pop();
+
+  }
+}
+class Particle2 {
+  constructor(x, y) {
+    this.dia = random(2, 4);
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(1, 0);
+    this.accadj = random(0.01, 0.4);
+    this.velReduction = .7;
+    
+
+  }
+
+  update() {
+    mouseVec = createVector(mouseX, mouseY);
+    this.acc = p5.Vector.sub(mouseVec, this.pos);
+    this.acc.mult(2);
+    this.vel.mult(this.velReduction);
+    this.acc.mult(this.accadj);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  
+
+  }
+  modify() {
+  this.pos=createVector(random(width/2-150,width/2+150),random(height/2-150,height/2+150));
+  this.acc=createVector(-.05,.05);
+    this.velocity= createVector(random(-10,10),random(-10,10));
+    this.vel.add(this.acc);
+    this.pos.add(this.velocity);
+    
+  }
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    noStroke();
+    fill(255, random(5,255));
+    rect(0, 0, this.dia, this.dia);
+
     pop();
 
   }
